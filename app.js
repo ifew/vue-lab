1a2ef76ebbd10bfcf89deeefc6cf79570657509d
admin <admin@MacBook-Pro.local>
2019-11-14 14:51:47 +0700

    Added grab news from NYTime API

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 window.onload = function() {
   const vm = new Vue({
-    el: "#app",
+    el: '#app',
     data: {
-      results: [
-        {
-          title: "the very first post",
-          abstract: "lorem ipsum some test dimpsum"
-        },
-        {
-          title: "and then there was the second",
-          abstract: "lorem ipsum some test dimsum"
-        },
-        {
-          title: "third time's a charm",
-          abstract: "lorem ipsum some test dimsum"
-        },
-        {
-          title: "four the last time",
-          abstract: "lorem ipsum some test dimsum"
-        }
-      ]
+      results: []
+    },
+    mounted() {
+      axios.get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=zDDMXw9t80Jlaa3bVOqQ23xNeMlwTy37")
+      .then(response => {this.results = response.data.results})
     }
   });
 };
