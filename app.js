b9a181a0d87038c67b36943ba2da3122c8667082
admin <admin@MacBook-Pro.local>
2019-11-14 14:55:26 +0700

    Refactoring

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 window.onload = function() {
-  const vm = new Vue({
-    el: '#app',
-    data: {
-      results: []
-    },
-    mounted() {
-      axios.get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=zDDMXw9t80Jlaa3bVOqQ23xNeMlwTy37")
-      .then(response => {this.results = response.data.results})
+//https://api.nytimes.com/svc/topstories/v2/home.json?api-key=zDDMXw9t80Jlaa3bVOqQ23xNeMlwTy37
+const NYTBaseUrl = "https://api.nytimes.com/svc/topstories/v2/";
+const ApiKey = "zDDMXw9t80Jlaa3bVOqQ23xNeMlwTy37";
+
+function buildUrl (url) {
+  return NYTBaseUrl + url + ".json?api-key=" + ApiKey
+}
+
+const vm = new Vue({
+  el: '#app',
+  data: {
+    results: []
+  },
+  mounted () {
+    this.getPosts('home');
+  },
+  methods: {
+    getPosts(section) {
+      let url = buildUrl(section);
+      axios.get(url).then((response) => {
+        this.results = response.data.results;
+      }).catch( error => { console.log(error); });
     }
-  });
+  }
+});
 };
